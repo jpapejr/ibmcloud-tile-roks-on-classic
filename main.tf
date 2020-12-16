56c948376186db7c96a0efc74c8d94f20da85442
jpapejr <jpapejr@icloud.com>
2020-12-16 16:28:13 -0500

    added storage removal flag to cluster

--- main.tf
+++ main.tf
@@ -30,13 +30,14 @@ data "ibm_resource_group" "resource_group" {
 }
 
 resource "ibm_container_cluster" "cluster" {
-  name              = "mycluster-${random_id.name1.hex}"
-  datacenter        = var.zone
-  default_pool_size = var.worker_count
-  machine_type      = var.cluster_node_flavor
-  hardware          = "shared"
-  kube_version      = var.cluster_kube_version
-  public_vlan_id    = ibm_network_vlan.public_vlan.id
-  private_vlan_id   = ibm_network_vlan.private_vlan.id
+  name                 = "mycluster-${random_id.name1.hex}"
+  datacenter           = var.zone
+  default_pool_size.   = var.worker_count
+  machine_type         = var.cluster_node_flavor
+  hardware             = "shared"
+  kube_version         = var.cluster_kube_version
+  public_vlan_id       = ibm_network_vlan.public_vlan.id
+  private_vlan_id      = ibm_network_vlan.private_vlan.id
+  force_delete_storage = true
 
 }
