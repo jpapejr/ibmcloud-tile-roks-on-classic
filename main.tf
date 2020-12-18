8e208f2779eb8ff5192991fdf1554291be6f4b71
jpapejr <jpapejr@icloud.com>
2020-12-18 15:47:12 -0500

    mzr push

--- main.tf
+++ main.tf
@@ -13,17 +13,46 @@ resource "random_id" "name3" {
 
 resource "ibm_network_vlan" "public_vlan" {
    name = "public-vlan-${random_id.name1.hex}"
-   datacenter = var.zone
+   datacenter = "wdc04"
    type = "PUBLIC"
-   router_hostname = "fcr01a.${var.zone}"
+   router_hostname = "fcr01a.wdc04"
 }
 
 resource "ibm_network_vlan" "private_vlan" {
    name = "private-vlan-${random_id.name1.hex}"
-   datacenter = var.zone
+   datacenter = "wdc04"
    type = "PRIVATE"
-   router_hostname = "bcr01a.${var.zone}"
+   router_hostname = "bcr01a.wdc04"
 }
+
+resource "ibm_network_vlan" "public_vlan2" {
+   name = "public-vlan-${random_id.name1.hex}"
+   datacenter = "wdc06"
+   type = "PUBLIC"
+   router_hostname = "fcr01a.wdc06"
+}
+
+resource "ibm_network_vlan" "private_vlan2" {
+   name = "private-vlan-${random_id.name1.hex}"
+   datacenter = "wdc06"
+   type = "PRIVATE"
+   router_hostname = "bcr01a.wdc06"
+}
+
+resource "ibm_network_vlan" "public_vlan3" {
+   name = "public-vlan-${random_id.name1.hex}"
+   datacenter = "wdc07"
+   type = "PUBLIC"
+   router_hostname = "fcr01a.wdc07"
+}
+
+resource "ibm_network_vlan" "private_vlan3" {
+   name = "private-vlan-${random_id.name1.hex}"
+   datacenter = "wdc07"
+   type = "PRIVATE"
+   router_hostname = "bcr01a.wdc07"
+}
+
 
 data "ibm_resource_group" "resource_group" {
   name = var.resource_group
@@ -31,7 +60,7 @@ data "ibm_resource_group" "resource_group" {
 
 resource "ibm_container_cluster" "cluster" {
   name                 = "${var.cluster_name}-${random_id.name1.hex}"
-  datacenter           = var.zone
+  datacenter           = wdc04
   default_pool_size    = var.worker_count
   machine_type         = var.cluster_node_flavor
   hardware             = "shared"
@@ -41,4 +70,52 @@ resource "ibm_container_cluster" "cluster" {
   force_delete_storage = true
   wait_till            = "OneWorkerNodeReady"
 
+}
+
+resource "ibm_container_worker_pool" "pool2" {
+  worker_pool_name = "default2"
+  machine_type     = var.cluster_node_flavor
+  cluster          = "${var.cluster_name}-${random_id.name1.hex}"
+  size_per_zone    = var.worker_count
+  hardware         = "shared"
+  disk_encryption  = "true"
+  
+}
+resource "ibm_container_worker_pool_zone_attachment" "pool2_za" {
+  cluster         = "${var.cluster_name}-${random_id.name1.hex}"
+  worker_pool     = element(split("/",ibm_container_worker_pool.pool2.id),1)
+  zone            = "wdc06"
+  private_vlan_id = ibm_network_vlan.private_vlan2.id
+  public_vlan_id  = ibm_network_vlan.public_vlan2.id
+
+  //User can increase timeouts
+  timeouts {
+      create = "90m"
+      update = "3h"
+      delete = "30m"
+    }
+}
+
+resource "ibm_container_worker_pool" "pool3" {
+  worker_pool_name = "default3"
+  machine_type     = var.cluster_node_flavor
+  cluster          = "${var.cluster_name}-${random_id.name1.hex}"
+  size_per_zone    = var.worker_count
+  hardware         = "shared"
+  disk_encryption  = "true"
+  
+}
+resource "ibm_container_worker_pool_zone_attachment" "pool3_za" {
+  cluster         = "${var.cluster_name}-${random_id.name1.hex}"
+  worker_pool     = element(split("/",ibm_container_worker_pool.pool3.id),1)
+  zone            = "wdc07"
+  private_vlan_id = ibm_network_vlan.private_vlan3.id
+  public_vlan_id  = ibm_network_vlan.public_vlan3.id
+
+  //User can increase timeouts
+  timeouts {
+      create = "90m"
+      update = "3h"
+      delete = "30m"
+    }
 }
